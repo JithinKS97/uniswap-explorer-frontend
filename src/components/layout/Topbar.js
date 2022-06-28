381db52ed17dd0118959e50e3d3b4bbf343853c0
JithinKS97 <jithinks112@gmail.com>
2022-06-29 00:11:43 +0530

    styles

--- src/components/layout/Topbar.js
+++ src/components/layout/Topbar.js
@@ -19,24 +19,26 @@ export default function Topbar(props) {
       height='9vh'
       backgroundColor='gray.900'
       padding={"1rem"}>
-      <Center>
-        <VStack position={"absolute"} left='1rem' top='1rem'>
-          <Box>Logged in with account</Box>
-          <Tooltip label={account}>
-            <Box cursor={"pointer"} fontSize={"12"}>
-              {trimmedAccount}
-            </Box>
-          </Tooltip>
-        </VStack>
+      <Center
+        display={"flex"}
+        flexDirection='column'
+        h='100%'
+        position={"absolute"}
+        top='0'
+        left='1rem'>
+        <Box>Logged in with account</Box>
+        <Tooltip label={account}>
+          <Box cursor={"pointer"} fontSize={"12"}>
+            {trimmedAccount}
+          </Box>
+        </Tooltip>
       </Center>
       <Center>
         <Heading>{heading}</Heading>
       </Center>
-      <Box>
-        <Button onClick={logout} position={"absolute"} right='1rem' top='1rem'>
-          Logout
-        </Button>
-      </Box>
+      <Center position={"absolute"} h='100%' top='0' right='1rem'>
+        <Button onClick={logout}>Logout</Button>
+      </Center>
     </Box>
   );
 }
