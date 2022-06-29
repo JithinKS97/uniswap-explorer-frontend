9d07cfa716da2e0457c760742681f9642198a099
JithinKS97 <jithinks112@gmail.com>
2022-06-30 01:20:14 +0530

    changed remote url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: "http://localhost:8000",
+  BASE_URL: "https://uniswap-explorer.herokuapp.com",
   ETHERSCAN_TX_BASE_URL: "https://ropsten.etherscan.io/tx",
   ETHERSCAN_ADDRESS_BASE_URL: "https://ropsten.etherscan.io/address",
 };
