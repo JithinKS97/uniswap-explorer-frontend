13d83dda7f51ae4152d476c011770f969ca3c1dd
JithinKS97 <jithinks112@gmail.com>
2022-07-01 12:26:10 +0530

    uniswap link

--- src/components/transactions/Filters/index.js
+++ src/components/transactions/Filters/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Button } from "@chakra-ui/react";
 import Menu from "./Menu";
+import Link from "next/link";
 
 export default function index(props) {
   const { selectedTime, onTimeChange, onRefreshClick } = props;
@@ -8,9 +9,17 @@ export default function index(props) {
   return (
     <Box
       display={"flex"}
-      justifyContent='right'
+      justifyContent='space-between'
       padding-bottom={"0"}
       padding='1.5rem 0'>
+      <Box>
+        <a
+          rel='noreferrer'
+          target={"_blank"}
+          href='https://ropsten.etherscan.io/address/0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'>
+          <Button>Uniswap contract</Button>
+        </a>
+      </Box>
       <Box>
         <Button onClick={onRefreshClick} marginRight='1rem'>
           Refresh
